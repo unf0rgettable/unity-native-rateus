bc77c86faccfc1fad5cc1aa6a0bc4dc321983993
Oleg Filimonov <fila20007@gmail.com>
2022-12-08 14:30:47 +0300

    release android native rate us 1.0.0

---
+++ Runtime/RateUsModule/RateUsService.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using Google.Play.Review;
+using LittleBit.Modules.CoreModule;
+using UnityEngine;
+
+namespace Unfo.Modules.RateUsModule
+{
+    public class RateUsService
+    {
+        private readonly ICoroutineRunner _coroutineRunner;
+        private readonly ReviewManager _reviewManager;
+        private PlayReviewInfo _playReviewInfo;
+
+        public RateUsService(ICoroutineRunner coroutineRunner)
+        {
+            _coroutineRunner = coroutineRunner;
+            _reviewManager = new ReviewManager();
+        }
+
+        private IEnumerator GetPlayReviewInfoWithShow()
+        {
+            var requestFlowOperation = _reviewManager.RequestReviewFlow();
+            yield return requestFlowOperation;
+            if (requestFlowOperation.Error != ReviewErrorCode.NoError)
+            {
+                Debug.LogError(requestFlowOperation.Error.ToString());
+                yield break;
+            }
+            _playReviewInfo = requestFlowOperation.GetResult();
+            Debug.LogWarning(_playReviewInfo.ToString());
+
+            var launchFlowOperation = _reviewManager.LaunchReviewFlow(_playReviewInfo);
+            
+            yield return launchFlowOperation;
+            
+            if (launchFlowOperation.Error != ReviewErrorCode.NoError)
+            {
+                Debug.LogError(launchFlowOperation.Error.ToString());
+                yield break;
+            }
+            Debug.LogWarning("Showing Rate Us");
+        }
+
+        public void ShowReviewWindowWithRequest()
+        {
+            _coroutineRunner.StartCoroutine(GetPlayReviewInfoWithShow());
+        }
+    }
+}
